aa7e6e86c1b7d6ff15be7cad66533c1b3023ed4e
FreakishLancer <destfinis@yahoo.com>
2016-12-11 00:53:18 -0500

    first commit

---
+++ Jokes/Jokes.py
@@ -0,0 +1,20 @@
+import random
+
+jokesNumber = random.randint(1, 3)
+
+if jokesNumber == 1:
+    print("What do you get when you cross a snowman with a vampire?")
+    input()
+    print("Frostbite!")
+    print()
+elif jokesNumber == 2:
+        print("What do dentists call an astronaut's cavity?")
+        input()
+        print("A black hole!")
+        print()
+elif jokesNumber == 3:
+    print("Knock, knock.")
+    input()
+    print("Interrupting cow.")
+    input()
+    print("-MOO!")
